b865971887788805e629dde7ddafff9d422bf094
georgelwalker <gwalker@escapesystems.com>
2016-12-21 16:13:50 -0800

    SB-51 - updates to support code changes

--- go-mvc.bat
+++ go-mvc.bat
@@ -1,10 +1,10 @@
 del mvc\*.* /s /q
-call generate-all.bat swagger.json mvc
+call generate-all.bat http://editor-tran-schoolbus-dev.pathfinder.gov.bc.ca/export/swagger.yaml mvc
 rem goto end1:
 
 cd mvc\src\SchoolBusAPI
-dotnet restore
-dotnet ef migrations add initial
+rem dotnet restore
+rem dotnet ef migrations add initial
 cd ..\..\..
 
 :end1
