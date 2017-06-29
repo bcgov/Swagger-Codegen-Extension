e42814afec14eeb3d88816ecfbe370837d86d7bb
georgelwalker <gwalker@escapesystems.com>
2017-06-29 09:51:28 -0700

    Updated generation batch file

--- generate-all.bat
+++ generate-all.bat
@@ -3,7 +3,7 @@ IF %1.==. GOTO No1
 IF %2.==. GOTO No2
 IF %3.==. GOTO No3
 
-set executable=target/aspnetCore-swagger-codegen-1.0.1-jar-with-dependencies.jar;swagger-codegen-cli.jar
+set executable=target/swagger-codegen-extension-1.0.1-jar-with-dependencies.jar;swagger-codegen-cli.jar
 set JAVA_OPTS=-Dfile.encoding=UTF-8 -DapiTests=true -DmodelTests=true
 set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
 set args=generate -i %1 -l aspnetmvc -c %3 -o %2
